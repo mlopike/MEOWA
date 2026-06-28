47075f51e6b5e088e1ea6169044cc033537a34f9
MLOP\i7760 <ilya.jakimovich@mail.ru>
2026-06-28 22:03:55 +0300

    Полностью функциональные новости

--- src/main/java/com/bsac/meowa/repo/BranchRateRepository.java
+++ src/main/java/com/bsac/meowa/repo/BranchRateRepository.java
@@ -1,10 +1,8 @@
 package com.bsac.meowa.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.bsac.meowa.model.BranchRate;
 
-@Repository
 public interface BranchRateRepository extends JpaRepository<BranchRate, Long> {
 }
