aa935251551e1896477c2bbb8fa090d11ee84e2e
MLOP\i7760 <ilya.jakimovich@mail.ru>
2026-06-28 21:42:09 +0300

    Убраны избыточные зависимости

--- src/main/java/com/bsac/meowa/repo/LocationRepository.java
+++ src/main/java/com/bsac/meowa/repo/LocationRepository.java
@@ -1,10 +1,8 @@
 package com.bsac.meowa.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.bsac.meowa.model.Location;
 
-@Repository
 public interface LocationRepository extends JpaRepository<Location, Integer> {
 }
